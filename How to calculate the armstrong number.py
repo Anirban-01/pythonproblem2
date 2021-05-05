d417f3ab3bb636020ac3c1d92f1674a476404af6
Python learner <areyaban@gmail.com>
2021-05-05 20:40:35 +0530

    Add files via upload

---
+++ How to calculate the armstrong number.py
@@ -0,0 +1,15 @@
+import math
+
+number = 153
+
+n1 = math.pow(1, 3)
+n2 = math.pow(5, 3)
+n3 = math.pow(3, 3)
+
+total_sum = n1 + n2 + n3
+
+if number == total_sum:
+    print('number is armstrong number')
+else:
+    print('number not armstrong number')
+
